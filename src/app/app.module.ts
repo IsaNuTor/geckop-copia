d970750aeb327fcd015c2c63609fe2f6a20d9613
Isabel <isanunez@ucm.es>
2019-02-18 19:19:17 +0100

    primera version con login

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+// Servicios
+import {AuthService} from './services/auth.service';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PerfilComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'perfil', component: PerfilComponent },
+    /*{ path: '', component: Component },
+    { path: '', component: Component },
+    { path: '', component: Component },
+    { path: '', component: Component },*/
+    { path: '**', pathMatch: 'full', redirectTo: 'home' } // Por defecto redirecciona a esta ruta si llega
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash: true});
