9f78cffa2189f0b7864e537f15975708d99d1d0a
Azahara Fernández <azaferna@ucme.s>
2019-03-04 11:40:16 +0100

    ordenes

---
+++ src/app/components/add-orden/add-orden.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-orden',
+  templateUrl: './add-orden.component.html',
+  styleUrls: ['./add-orden.component.css']
+})
+export class AddOrdenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
