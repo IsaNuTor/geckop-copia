467ff13ff17d7f5afb02e238f73b081d2aecef02
Azahara Fernández <azaferna@ucme.s>
2019-03-12 13:45:05 +0100

    tabla funcionando

--- src/app/components/tabla-ordenes/tabla-ordenes.component.ts
+++ src/app/components/tabla-ordenes/tabla-ordenes.component.ts
@@ -8,14 +8,55 @@ import * as $ from 'jquery';
 export class TablaOrdenesComponent implements OnInit {
 
   constructor() { }
-
+ 	
   ngOnInit() {
-  }
+		 $("#numeracion").keyup(
+		 		function(){
+					 //alert(numeracion);
+					var numeracion = String($("#numeracion").val());
+					//var filter = numeracion.value.toUpperCase();
+					var tr = $('#tablaOrdenes tbody').children('tr');
+					 
+					   // Loop through all table rows, and hide those who don't match the search query
+				 var i = 0;
+				 for (i = 0; i < tr.length; i++) {
+						var td = tr[i].getElementsByTagName("td")[1];
+						if (td) {
+							var txtValue = td.textContent || td.innerText;
+							if (txtValue.indexOf(numeracion) > -1) {
+								tr[i].style.display = "";
+							} else {
+								tr[i].style.display = "none";
+							}
+						} 
+				 }
+		 });
+		 
+		 
+		  /*var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
 
- 
-  $(document).ready(function() {
-  $(".search").keyup(function () {
-    var searchTerm = $(".search").val();
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}*/
+	}
+		 
+		/*  $(".search").keyup(function () {
+    //var searchTerm = $(".search").val();
+		var searchTerm = "id1";
     var listItem = $('.results tbody').children('tr');
     var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
     
@@ -38,7 +79,12 @@ export class TablaOrdenesComponent implements OnInit {
   if(jobCount == '0') {$('.no-result').show();}
     else {$('.no-result').hide();}
 		  });
-	});
+  }*/
+	 
+	 
+}
+ 
+
   
 
-}
+
