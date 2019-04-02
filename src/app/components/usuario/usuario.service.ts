681e56f663dc30a467a666794ad61d0289f5f82e
Azahara <azaferna@ucm.es>
2019-04-02 17:49:50 +0200

    Registro funcionando

--- src/app/components/usuario/usuario.service.ts
+++ src/app/components/usuario/usuario.service.ts
@@ -10,16 +10,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export class UsuarioService {
 
-  private urlEndPoint:string = 'http://localhost:8080/api/usuario';
+  private urlLogin:string = 'http://localhost:8080/api/login';
+  private urlRegistro:string = 'http://localhost:8080/api/registro';
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
   constructor(private http: HttpClient) { }
 
   // Nos devuelve los clientes.
   public login(usuario:Usuario): Observable<Usuario> {
-     return this.http.post<Usuario>(this.urlEndPoint, usuario,  {headers: this.httpHeaders});
+     return this.http.post<Usuario>(this.urlLogin, usuario,  {headers: this.httpHeaders});
   }
   // Nos devuelve los clientes.
   public registro(usuario:Usuario): Observable<Usuario> {
-     return this.http.put<Usuario>(this.urlEndPoint, usuario,  {headers: this.httpHeaders});
+     return this.http.post<Usuario>(this.urlRegistro, usuario,  {headers: this.httpHeaders});
   }
 }
